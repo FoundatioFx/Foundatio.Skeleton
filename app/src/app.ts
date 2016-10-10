733ede8bb51570330b602291ac46c79afe98bfd0
ejsmith <eric@codesmithtools.com>
2016-10-10 14:32:38 -0500

    Trying to build out skeleton in materialize.

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,14 +1,15 @@
-import {Router, RouterConfiguration} from 'aurelia-router';
+import {Router, RouterConfiguration} from "aurelia-router";
 
 export class App {
-  public router: Router;
-  message = 'Hello World!';
+  router: Router;
 
-  public configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = "Aurelia";
+    config.options.pushState = true;
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' }
+      { route: ["", "welcome"], name: "welcome",      moduleId: "welcome",      nav: true, title: "Welcome" },
+      { route: "users",         name: "users",        moduleId: "users",        nav: true, title: "Github Users" },
+      { route: "child-router",  name: "child-router", moduleId: "child-router", nav: true, title: "Child Router" }
     ]);
 
     this.router = router;
---
+++ app/src/users.ts
@@ -0,0 +1,27 @@
+import {autoinject} from "aurelia-framework";
+import {HttpClient} from "aurelia-fetch-client";
+
+interface IUser {
+  avatar_url: string;
+  login: string;
+  html_url: string;
+}
+
+@autoinject
+export class Users {
+  heading: string = "Github Users";
+  users: Array<IUser> = [];
+
+  constructor(public http: HttpClient) {
+    http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl("https://api.github.com/");
+    });
+  }
+
+  async activate() {
+    let res = await this.http.fetch("users");
+    this.users = await res.json();
+  }
+}
